e8db23626282a3da21db97dae4bb9c7744b1ef02
Manuel Pereira <manuelgpereira@tecnico.ulisboa.pt>
2024-08-15 09:00:31 +0100

    Add holidays api

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express  from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { createClientAsync } from 'soap';
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
 const app = express();
 
 app.use(cors());
+
+const holidayUrl = 'http://services.sapo.pt/Holiday?wsdl';
 
 app.get("/api/benficaHomeScheduledGames", async (req, res) => {
     const response = await fetch(
@@ -22,6 +25,19 @@ app.get("/api/benficaHomeScheduledGames", async (req, res) => {
     res.json(await response.json())
 });
 
+app.get("/api/holidays/:year", async (req, res) => {
+    try {
+        const client = await createClientAsync(holidayUrl);
+        const [result] = await client.GetAllHolidaysAsync({year: parseInt(req.params.year)});
+        res.json(result);
+    }
+    catch(error){
+        console.error('Error fetching holidays:', error);
+        res.status(500).json({ error: 'An error occurred while fetching holidays' });
+    }
+})
+
+
 app.listen(8080, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port 8080");
 })
